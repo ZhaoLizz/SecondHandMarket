1ef559136a61fdb742f0a3d3b7803e994c284395
Qinchuji <qinbattlenet@163.com>
2018-03-17 21:22:34 +0800

    1

--- app/src/main/java/com/market/secondhandmarket/MainActivity.java
+++ app/src/main/java/com/market/secondhandmarket/MainActivity.java
@@ -4,13 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.getbase.floatingactionbutton.FloatingActionButton;
-import com.orhanobut.logger.Logger;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
     FloatingActionButton mPublishSale;
     @BindView(R.id.publish_tobuy)
     FloatingActionButton mPublishTobuy;
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
+    private SellFragment mSellFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -30,10 +37,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-
+                case R.id.navigation_dashboard:     //列表
+                    replaceFragment(mSellFragment);
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.navigation_home:
                     return true;
                 case R.id.navigation_notifications:
                     return true;
@@ -47,7 +54,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mSellFragment = SellFragment.newInstance();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_coninter, mSellFragment)
+                .show(mSellFragment)
+                .commit();
+
     }
 
 
@@ -58,8 +74,30 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(this, PublishActivity.class));
                 break;
             case R.id.publish_tobuy:
-
+                startActivity(new Intent(this, ToBuyActivity.class));
                 break;
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search_view, menu);
+
+        //找到searchView
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+//        searchView
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    private void replaceFragment(Fragment fragment) {
+        Fragment curFragment = getSupportFragmentManager().findFragmentById(R.id.fragment_coninter);
+        getSupportFragmentManager().beginTransaction()
+                .hide(curFragment)
+                .show(fragment)
+                .commit();
+    }
+
 }
